b9bf9778629926368eb3331bfa92f0da8ecae550
Crenshaw <lazerspam101@gmail.com>
2021-04-09 21:02:56 -0700

    Connect setting

--- commands/joke.js
+++ commands/joke.js
@@ -3,10 +3,13 @@ const {getRedditPost} = require("../funcs");
 module.exports = {
     command: "joke",
     description: "Self Explanatory",
-    executor: async () => {
+    executor: async (args, main) => {
 
         const subReddits = ["jokes", "dadjokes", "antijokes", "meanjokes"]
         let post = await getRedditPost(subReddits);
+
+        let footer;
+        if (main.settings.get('showRedditData')) footer = {text: `👍 ${post.ups} 💬 ${post.num_comments} - ${post.subreddit_name_prefixed}`}
 
         return {
             send: false,
@@ -15,9 +18,7 @@ module.exports = {
                 title: post.title,
                 color: 0x4B0082,
                 description: `||${post.selftext} ||`,
-                footer: {
-                    text: `👍 ${post.ups} 💬 ${post.num_comments} - ${post.subreddit_name_prefixed}`
-                }
+                footer: footer
             }
         }
 
--- commands/meme.js
+++ commands/meme.js
@@ -3,11 +3,14 @@ const {getRedditPost, imageWH} = require("../funcs");
 module.exports = {
     command: "meme",
     description: "Self Explanatory",
-    executor: async () => {
+    executor: async (args, main) => {
 
         const subReddits = ["dankmeme", "meme", "me_irl"];
         let post = await getRedditPost(subReddits);
         let wh = await imageWH(post.url_overridden_by_dest)
+
+        let footer;
+        if (main.settings.get('showRedditData')) footer = {text: `👍 ${post.ups} 💬 ${post.num_comments} - ${post.subreddit_name_prefixed}`}
 
         return {
             send: false,
@@ -19,9 +22,7 @@ module.exports = {
                     width: wh.width,
                     height: wh.height
                 },
-                footer: {
-                    text: `👍 ${post.ups} 💬 ${post.num_comments} - ${post.subreddit_name_prefixed}`
-                }
+                footer: footer
             }
         }
 
